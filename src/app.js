f1de3a4181e432ab99d4019842c15013571b971c
Rafael Souza <rafasouza2509@gmail.com>
2020-06-15 18:05:59 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -9,25 +8,86 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+const likes = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const indexRepo = repositories.findIndex(repo => repo.id === id);
+
+  if(indexRepo < 0){
+    return response.status(400).json({ error: 'Invalid repositorie ID!'});
+  }
+
+  const repo = {
+    id: repositories[indexRepo].id,
+    title,
+    url,
+    techs,
+    likes: repositories[indexRepo].likes
+  }
+
+  repositories[indexRepo] = repo;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const indexRepo = repositories.findIndex(repo => repo.id === id);
+
+  if(indexRepo < 0){
+    return response.status(400).json({ error: 'Invalid repositorie ID!'});
+  }
+
+  repositories.splice(indexRepo, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:idRepo/like", (request, response) => {
+  const { idRepo } = request.params;
+
+  const indexRepo = repositories.findIndex(repo => repo.id === idRepo);
+
+  if(indexRepo < 0){
+    return response.status(400).json({ error: 'Invalid repositorie ID!'});
+  }
+
+  const like = {
+    idRepo,
+    idUser: uuid()
+  }
+
+  repositories[indexRepo].likes += 1;
+
+  likes.push(like);
+
+  return response.json(repositories[indexRepo]);
+});
+
+app.get("/repositories/:idRepo/like", (request, response) => {
+  const { idRepo } = request.params;
+
+  const listLikes = likes.filter(like => like.idRepo === idRepo);
+
+  return response.json(listLikes);
 });
 
 module.exports = app;
